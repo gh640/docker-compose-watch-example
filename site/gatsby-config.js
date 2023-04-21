11568d6e1aea9cf511408c4ac08fe4e8d33c5180
Goto Hayato <gh640@hey.com>
2023-04-21 16:40:31 +0900

    Add files in `site/`

---
+++ site/gatsby-config.js
@@ -0,0 +1,11 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `My Gatsby Site`,
+    siteUrl: `https://www.yourdomain.tld`,
+    description: `This is a description text.`,
+  },
+  plugins: [],
+}
